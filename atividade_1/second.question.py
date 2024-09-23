20e216f2c9aa4dd0ff8776f64d4e7aa5b04f5ab2
matheusmatta21 <matheusalexandre2004@gmail.com>
2024-09-22 22:08:25 -0300

    feat: finished second question

--- atividade_1/second.question.py
+++ atividade_1/second.question.py
@@ -7,21 +7,34 @@ import scipy.stats as scp
 import seaborn as sns
 import pandas as pd
 
+iris = pd.read_csv('Iris.csv')
+
+species_list = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+columns_list = ['SepalLengthCm', 'PetalLengthCm']
+
+#Essa função cria e mostra gráficos de dispersão
+
 def show_scatterplot(data, x_col, y_col):
     sns.scatterplot(data=data, x=x_col, y=y_col)
     plt.show()
 
+#Essa função calcula e imprime valores de correlação de Pearson
+
 def find_pearson_correlation(x, y):
     correlation, p_value = scp.pearsonr(x, y)
-    print(f'spearman correlation coefficient for {x} and {y} : ', correlation)
-    print('proof value: ', p_value)
+    print(f'Coeficiente de correlação de Pearson: ', correlation)
+    print('Valor de prova: ', p_value)
+
+#Essa função calcula e imprime valores de correlação de Spearman
 
 def find_spearman_correlation(x, y):
     correlation, p_value = scp.spearmanr(x, y)
-    print(f'spearman correlation coefficient for {x} and {y} : ', correlation)
-    print('proof value: ', p_value)
+    print(f'Coeficiente de correlação de Spearman: ', correlation)
+    print('Valor de prova: ', p_value)
 
-def process_iris_data(iris, species, x_col, y_col): #Essa função processa dados de uma determinada espécie de iris e exibe gráficos de dispersão e correlações de Pearson.
+#Essa função processa dados de uma determinada espécie de iris e exibe gráficos de dispersão e correlações de Pearson e Spearman.
+
+def process_iris_data(iris, species, x_col, y_col):
     # Filtra os dados pela espécie
     iris_species = iris[iris['Species'] == species]
     
@@ -29,11 +42,17 @@ def process_iris_data(iris, species, x_col, y_col): #Essa função processa dado
     x = iris_species[x_col]
     y = iris_species[y_col]
     
-    # Cria o gráfico de dispersão
-    show_scatterplot(iris_species, x_col, y_col)
-    
     # Calcula a correlação de Pearson
-    correlation, p_value = scp.pearsonr(x, y)
-    print(f"Correlação de Pearson entre {x_col} e {y_col} para {species}: {correlation} (p-value: {p_value})")
-    correlation, p_value = scp.spearmanr(x, y)
-    print(f"Correlação de Spearman entre {x_col} e {y_col} para {species}: {correlation} (p-value: {p_value})")
+    find_pearson_correlation(x, y)
+
+    # Calcula a correlação de Spearman
+    find_spearman_correlation(x, y)
+
+    # Cria e mostra o gráfico de dispersão
+    show_scatterplot(iris_species, x_col, y_col)
+
+#Execução principal do programa
+
+for specie in species_list:
+    print(f'\nCorrelação entre {columns_list[0]} e {columns_list[1]} em relação à especie {specie}: \n')
+    process_iris_data(iris, specie, columns_list[0], columns_list[1])
